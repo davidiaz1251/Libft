d5c5648349fd95c03edd805cabad284fb54448ce
Luis David <ldiaz-ra@c2r9s1.42madrid.com>
2023-10-09 11:55:30 +0200

    ultima version

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldiaz-ra <ldiaz-ra@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 15:10:29 by ldiaz-ra          #+#    #+#             */
-/*   Updated: 2023/10/02 20:45:25 by ldiaz-ra         ###   ########.fr       */
+/*   Updated: 2023/10/09 11:53:13 by ldiaz-ra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		new_node = ft_lstnew(value);
 		if (!new_node)
 		{
-			del(new_node);
+			del(value);
 			ft_lstclear(&new_list, (*del));
-			return (new_list);
+			return (NULL);
 		}
 		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
